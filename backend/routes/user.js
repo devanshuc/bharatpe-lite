dfae752765330733f76794356c27507011b07fa4
Devanshu <devanshuchi9@gmail.com>
2024-06-01 00:08:17 +0530

    Added update user function

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,15 @@
 // backend/routes/user.js
 const express = require("express");
+const { authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 const z = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("../middleware/middleware");
 const { JWT_SECRET } = "../config";
 
+//Signup
 const signUpBody = z.object({
   username: z.string().email(),
   f_name: z.string(),
@@ -40,6 +43,8 @@ router.post("./signup", async (req, res) => {
 
   res.json({ message: "User successfully created.", token: token });
 });
+
+//Login
 const loginBody = z.object({
   username: z.string().email(),
   password: z.string(),
@@ -74,6 +79,27 @@ router.post("./login", async (req, res) => {
 
   res.status(411).json({
     message: "Error while logging in",
+  });
+});
+
+//Update User
+
+const updateBody = z.object({
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  password: z.string(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({ message: "Error while updating" });
+  }
+
+  await User.updateOne({ _id: req.userId }, req.body);
+
+  res.json({
+    message: "Updated successfully",
   });
 });
 
